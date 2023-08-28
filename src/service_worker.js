0d308e8164dfbfd6b4e4f46043f815809243809a
zhangpan <zhangpanrobot@gmail.com>
2023-08-29 01:23:04 +0800

    dbtable

--- src/service_worker.js
+++ src/service_worker.js
@@ -1,7 +1,8 @@
 import { initEventHandler } from '@/utils/extension-action'
 import supabaseClient from '@/lib/supabase'
 
-const dbClient = supabaseClient.from('pagination_selector')
+const dbTable = supabaseClient.from('pagination_selector')
+
 const contentReq = {
   'to-get-pattern': toGetPattern,
   'to-save-detect-ele': toSaveDetectEle,
@@ -17,7 +18,7 @@ async function toGetPattern({ forceUpdate = false, domain }, sendResponse) {
   localPatternList = localPatternList || []
   domain_list = domain_list || []
   const domainTarget = domain_list.find((item) => item === domain)
-  const domainPattern = localPatternList.find((item) => item === domain)
+  const domainPattern = localPatternList.find((item) => item.domain === domain)
   if (
     !forceUpdate &&
     localPatternList.length &&
@@ -30,21 +31,20 @@ async function toGetPattern({ forceUpdate = false, domain }, sendResponse) {
     return
   }
   const [{ data: patternList }, { data: domainList }] = await Promise.all([
-    dbClient.select('domain,prev_selector,next_selector,updated_at').in([domain || '*']),
-    dbClient.select('domain'),
+    dbTable.select('domain,prev_selector,next_selector,updated_at').in('domain', domain ? [domain, '*'] : ['*']),
+    dbTable.select('domain'),
   ])
-  console.log(patternList, 'patternList')
-  sendResponse && sendResponse(patternList[0])
+  sendResponse && sendResponse(patternList.find((item) => item.domain === domain || '*'))
   chrome.storage.local.set({ pattern_list: patternList, domain_list: domainList, pattern_list_updated_at: Date.now() })
 }
 
 async function toSaveDetectEle(params) {
-  const paginationRes = await dbClient.select('domain').eq('domain', params.domain)
+  const paginationRes = await dbTable.select('domain').eq('domain', params.domain)
   if (paginationRes.data.length) {
-    const { data, error } = await dbClient.update(params).eq('domain', params.domain)
+    const { data, error } = await dbTable.update(params).eq('domain', params.domain)
     console.log(data, error, 'update')
   } else {
-    const { data, error } = await dbClient.insert(params)
+    const { data, error } = await dbTable.insert(params)
     console.log(data, error, 'insert')
   }
 }
