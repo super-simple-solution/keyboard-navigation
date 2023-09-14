5bf835d0c8268c42a5202e7f636d6d000fdfca05
wudaxue <1203588983@qq.com>
2023-09-14 18:48:51 +0800

    ignore specific websites, ignore when user editing

--- src/content/util.ts
+++ src/content/util.ts
@@ -18,13 +18,29 @@ export function setKeypad(pattern: PatternData) {
 // [class^='value' i] i or s
 // https://postgrest.org/en/stable/releases/v10.2.0.html
 function keypad(e: KeyboardEvent, pattern: PatternData) {
-  if (!pattern) return
-  const prevEle = getEle(pattern.prev_selector.join())
-  const nextEle = getEle(pattern.next_selector.join())
+  if (userEditing() || !pattern) return
+  // selector priority
+  const prevEle = getElBySelectorList(pattern.prev_selector)
+  const nextEle = getElBySelectorList(pattern.next_selector)
   if (keyCodeMap.left === e.code) {
     prevEle && prevEle.click()
   } else if (keyCodeMap.right === e.code) {
     nextEle && nextEle.click()
     console.log(nextEle, 'nextEle')
   }
+}
+
+function getElBySelectorList(list: string[]) {
+  let elRes
+  for (const selector of list) {
+    elRes = getEle(selector)
+    if (elRes) break
+  }
+  return elRes
+}
+
+function userEditing() {
+  const activeTarget = document.activeElement
+  if (!activeTarget || activeTarget.tagName === 'BODY') return false
+  return true
 }
